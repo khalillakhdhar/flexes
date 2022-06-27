20178127670fbd6ec3e84314b471151a3fa7d10b
khalil <khalillakhdharatc@gmail.com>
2022-06-27 15:19:05 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React, { Component } from 'react';
+import { StyleSheet, View,Text } from 'react-native';
+export default class HeightWidth extends Component {
+  render() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+    <Text> here is the container</Text>
+  <View style={styles.powderblue} />
+  <View style={styles.skyblue} />
+  <View style={styles.steelblue} />
+  <Text> ending the container</Text>
+  </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+  }
+  const styles = StyleSheet.create({
+    container:{
+    flex:1
+    },
+    powderblue:{
+    flex:1,
+    backgroundColor: 'powderblue',
+    },
+    skyblue:{
+    flex:2,
+    backgroundColor: 'skyblue',
+    },
+    steelblue:{
+    flex:3,
+    backgroundColor: 'steelblue',
+    },
+    })
